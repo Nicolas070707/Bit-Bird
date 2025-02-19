047c7405b0a484b44c41ef4fd8cc018186b5468e
Nicolas070707 <nicolas.theiner@student.htldornbirn.at>
2025-02-19 17:47:24 +0100

    Yeah

--- Bit-Bird/vite.config.js
+++ Bit-Bird/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
+import envCompatible from 'vite-plugin-env-compatible'
 
+// Vite-Konfiguration mit dem Plugin
 export default defineConfig({
   build: {
     lib: {
       entry: './lib/main.js',
       name: 'Counter',
       fileName: 'counter',
-    },
+    }
   },
+  plugins: [envCompatible()],
 })
